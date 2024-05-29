546408dc382718abf4e59bb669b3e64bd9a9b9d5
Njoki Ndichu <rejoicenjoki0@gmail.com>
2024-05-29 19:58:38 +0300

    statehook

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isAdded, setIsAdded] = useState(false);
+
+  function handleClick() {
+    setIsAdded(!isAdded);
+  }
+
+  const buttonText = isAdded ? "Remove from Cart" : "Add to Cart";
+  const liClassName = isAdded ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={liClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>
+        {buttonText}
+      </button>
     </li>
   );
 }
